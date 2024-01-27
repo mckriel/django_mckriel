5686d68256b4f5cb029af8692602e1670ade9c7f
mckriel <mckriel@gmail.com>
2024-01-27 15:18:25 +0200

    Added new entries to the db, tested some view.py changes

--- habitat/urls.py
+++ habitat/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("get-lineup-info", views.get_lineup_info, name='get_lineup_info'),
+    path("get_lineup_info/", views.get_lineup_info, name='get_lineup_info'),
+    path("get_lineup_info/<int:event_id>/", views.get_lineup_info, name='get_lineup_info_with_event_id'),
 ]
--- habitat/views.py
+++ habitat/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from django.core import serializers
-from .models import Lineup
+from .models import Lineup, Event
 
 
-def get_lineup_info(event_id=None):
-    results = Lineup.objects.all()
+# Get the lineup data, with an optional filter by event_id
+def get_lineup_info(request, event_id=None):
+    # If provided, filter lineup based on event ID
+    if event_id:
+        event = get_object_or_404(Event, pk=event_id)
+        results = Lineup.objects.filter(event=event)
+    # If event_id not provided, get all lineup data
+    else:
+        results = Lineup.objects.all()
+
+    # Serialize and return the data in JSON format
     json_data = serializers.serialize('json', results)
     return HttpResponse(json_data)
